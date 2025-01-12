f18a292ba275818b9cdd78fe9dbed6e3dbc5e640
EC2 Default User <ec2-user@ip-172-31-32-218.ap-northeast-1.compute.internal>
2025-01-12 10:42:00 +0000

    0112_postwork_system_installed

--- app/controllers/post_works_controller.rb
+++ app/controllers/post_works_controller.rb
@@ -5,10 +5,26 @@ class PostWorksController < ApplicationController
     @post_work_new = PostWork.new
   end
 
+  def create
+    post_work = PostWork.new(post_work_params)
+    project = Project.find(params[:project_id])
+    post_work.project_id = project.id
+    post_work.user_id = current_user.id
+    post_work.save
+    redirect_to project_post_works_path
+  end
+
   def index
   end
 
   def show
+  end
+
+
+  private
+
+  def post_work_params 
+    params.require(:post_work).permit(:name, :work, :area, :start_at, :end_at)
   end
 
 end
