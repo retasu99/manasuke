e3256a84f9dc6ddfbfe33a5c32b53af2ecb9c84d
EC2 Default User <ec2-user@ip-172-31-32-218.ap-northeast-1.compute.internal>
2025-01-12 07:56:46 +0000

    0112_PostWork_viewfile_new_made

--- app/controllers/post_works_controller.rb
+++ app/controllers/post_works_controller.rb
@@ -1,2 +1,14 @@
 class PostWorksController < ApplicationController
+
+  def new
+    @project_new = Project.find(params[:project_id])
+    @post_work_new = PostWork.new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
 end
