7bac06da3286b9808d1fa63890c1aba5facbca2b
EC2 Default User <ec2-user@ip-172-31-32-218.ap-northeast-1.compute.internal>
2025-01-12 02:48:15 +0000

    0112_Project_model_generated_and_migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_01_12_003605) do
+ActiveRecord::Schema.define(version: 2025_01_12_024435) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
