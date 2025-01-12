edea83b6d2ad9ce60596546972e1c5c5a025fca7
EC2 Default User <ec2-user@ip-172-31-32-218.ap-northeast-1.compute.internal>
2025-01-12 03:02:36 +0000

    0112_projects_controller_generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  # get 'projects/index'
+  resources :projects, only: [:index]
+
   devise_for :users
+
   # get 'homes/top'
   root to: "homes#top"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
