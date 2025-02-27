3e0896199b89a63616d1d32fda4baf68ef4dd1b2
EC2 Default User <ec2-user@ip-172-31-32-218.ap-northeast-1.compute.internal>
2025-02-27 19:55:04 +0000

    0228_project_index_layouted

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,8 +41,10 @@ example = Admin.find_or_create_by!(email: "admin@example.com") do |admin|
   admin.password = "adminpasuwado"
 end
 
-Project.find_or_create_by!(name: "project-01")
-Project.find_or_create_by!(name: "project-02")
+5.times{|i|Project.find_or_create_by!(name: "project-#{i}")}
+Project.find_or_create_by!(name: "すごいproject")
+Project.find_or_create_by!(name: "絶対成功させたいproject")
+Project.find_or_create_by!(name: "失敗したら弊社が潰れるproject")
 
 30.times{|i|PostWork.find_or_create_by!(project_id: "1", user_id: "2", name: "work#{i}", work: "test_work#{i}", area: "test_area#{i}", start_time: Time.current.since(i*2.days), end_time: Time.current.since(i*2.days))}
 
