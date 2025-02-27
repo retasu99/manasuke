9fb75bfd451dbd14af9534f697f5eca6464f83dc
EC2 Default User <ec2-user@ip-172-31-32-218.ap-northeast-1.compute.internal>
2025-02-27 05:21:37 +0000

    0227_Ajax_for_follow_installed

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,19 +1,19 @@
 class Public::RelationshipsController < ApplicationController
 
   def create
-    user = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
     relationship = Relationship.new
     relationship.follower_id = current_user.id
-    relationship.followed_id = user.id
+    relationship.followed_id = @user.id
     relationship.save
-    redirect_to user_path(user.id)
+#    redirect_to user_path(user.id)
   end
 
   def destroy
-    user = User.find(params[:user_id])
-    relationship = Relationship.find_by(follower_id: current_user.id, followed_id: user.id)
+    @user = User.find(params[:user_id])
+    relationship = Relationship.find_by(follower_id: current_user.id, followed_id: @user.id)
     relationship.destroy
-    redirect_to user_path(user.id)
+#    redirect_to user_path(user.id)
   end
 
 #  def following_users
