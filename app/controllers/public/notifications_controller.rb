426b2a1c7f6ee8657645dac27226ecc53f2f38f7
EC2 Default User <ec2-user@ip-172-31-32-218.ap-northeast-1.compute.internal>
2025-02-27 07:44:14 +0000

    0227_last_layouting

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,9 +1,6 @@
 class Public::NotificationsController < ApplicationController
   def index
     @notifications = current_user.notifications.order(created_at: :desc)
-    @notifications.where(checked: false).each do |notification|
-      notification.update(checked: true)
-    end
   end
 
   def destroy
