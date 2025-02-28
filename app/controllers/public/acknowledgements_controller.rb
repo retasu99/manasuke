ba3a97497598380471740bce743001232c7aeabc
EC2 Default User <ec2-user@ip-172-31-32-218.ap-northeast-1.compute.internal>
2025-02-28 07:34:56 +0000

    0228_user_pages_layouted

--- app/controllers/public/acknowledgements_controller.rb
+++ app/controllers/public/acknowledgements_controller.rb
@@ -6,7 +6,6 @@ class Public::AcknowledgementsController < ApplicationController
     acknowledgement = Acknowledgement.new(post_work_id: @post_work.id)
     acknowledgement.user_id = current_user.id
     acknowledgement.save
-#    redirect_to project_post_work_path(@project.id, @post_work.id)
   end
 
   def destroy
@@ -14,7 +13,6 @@ class Public::AcknowledgementsController < ApplicationController
     @post_work = PostWork.find(params[:post_work_id])
     acknowledgement = Acknowledgement.find_by(user_id: current_user.id, post_work_id: params[:post_work_id])
     acknowledgement.destroy
-#    redirect_to project_post_work_path(@project.id, @post_work.id)
   end
   
 end
